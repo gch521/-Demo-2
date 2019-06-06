d6314c34c4541167b9cb5b21bf6218e9aedc4010
sycm <>
2019-06-06 13:35:04 +0800

    1

--- ggggg/ggggg/ViewController.m
+++ ggggg/ggggg/ViewController.m
@@ -18,6 +18,37 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor greenColor];
+    /*
+     #工程名
+     APP_NAME="ggggg"
+     #目标名
+     TARGET_NAME="ggggg"
+     # 项目绝对路径
+     PROJECT_PWD_PATH="$PWD/ggggg/ggggg.xcodeproj"
+     # build 路径
+     BUILD_PATH="$PWD/build/Release-iphoneos"
+     # configuration
+     CONFIGURATION_TYPE="Debug"
+     # info.plist路径
+     INFO_PLIST_PATH="$PWD/ggggg/ggggg/info.plist"
+     # 签名
+     CODE_SIGN_NAME="iPhone Distribution:Zhejiang Shuyun Internet Media Co.,LTD"
+     # 描述文件
+     MOBILE_PROVISION="d1ddca74-85d7-4350-9158-8f36fed074d4.mobileprovision"
+     # 取版本号
+     bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "${INFO_PLIST_PATH}")
+     # 取build值
+     bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${INFO_PLIST_PATH}")
+     # 日期
+     DATE="$(date +%Y%m%d)"
+     # 生成.ipa名称
+     IPANAME="${APP_NAME}_V${bundleShortVersion}_${DATE}.ipa"
+     # .ipa路径
+     IPA_PATH="$PWD/text/build/${IPANAME}"
+     # remove Build_Directory
+     rm -rf "$PWD/ggggg/build"
+     xcodebuild clean -project ${PROJECT_PWD_PATH} -configuration ${CONFIGURATION_TYPE} -target ${APP_NAME} -quiet || exit
+     */
 }
 
 
